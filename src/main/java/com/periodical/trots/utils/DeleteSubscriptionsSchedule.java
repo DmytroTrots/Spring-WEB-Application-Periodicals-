b6119b6ce6b7eb3b4ffdb04226f5d408b2e28375
DmytroTrots <dtrots27@gmail.com>
2022-04-10 21:02:37 +0300

    clean up

--- src/main/java/com/periodical/trots/utils/DeleteSubscriptionsSchedule.java
+++ src/main/java/com/periodical/trots/utils/DeleteSubscriptionsSchedule.java
@@ -1,15 +1,17 @@
 package com.periodical.trots.utils;
 
 import com.periodical.trots.services.PeriodicalHasReceiptService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DeleteSubscriptionsSchedule {
 
-    @Autowired
-    private PeriodicalHasReceiptService periodicalHasReceiptService;
+    private final PeriodicalHasReceiptService periodicalHasReceiptService;
+
+    public DeleteSubscriptionsSchedule(PeriodicalHasReceiptService periodicalHasReceiptService) {
+        this.periodicalHasReceiptService = periodicalHasReceiptService;
+    }
 
     @Scheduled(cron = "00 00 22 1 * *", zone = "UTC")
     public void run() {
